2a608df82d8388e1c63051524db611242f472a52
rufus-stone <60827786+rufus-stone@users.noreply.github.com>
2020-04-07 10:21:12 +0100

    Setting up empty challenges. Configuring for C++17

---
+++ include/set_1/all.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "challenge_01.hpp"
+#include "challenge_02.hpp"
+#include "challenge_03.hpp"
+#include "challenge_04.hpp"
---
+++ include/set_1/challenge_01.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace set1::challenge1
+{
+
+using namespace std::string_literals;
+
+////////////////////////////////////////////////
+void run()
+{
+  auto data = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"s;
+
+
+}
+
+} // namespace set1::challenge1
