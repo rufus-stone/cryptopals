bfaacd631bbb46406d774cf4979ebe7f5d20332f
rufus-stone <60827786+rufus-stone@users.noreply.github.com>
2020-04-30 08:33:12 +0100

    AES ECB mode working. Set 1 Challenge 7 complete!

--- include/set_1/challenge_07.hpp
+++ include/set_1/challenge_07.hpp
@@ -11,20 +11,24 @@ namespace set_01::challenge_07
 auto aes_ecb_decrypt(std::string_view input, std::string_view key)
 {
   int len = input.size();
+  assert(len % 16 == 0);
 
   assert(key.size() == 16);
 
   auto ciphertext_ptr = reinterpret_cast<const uint8_t *>(input.data());
   auto key_ptr = reinterpret_cast<const uint8_t *>(key.data());
 
-  auto output = std::vector<uint8_t>(len, 0x00);
+  auto output = std::vector<uint8_t>(len, 0x00); // We have to initialise the vector with something to start with
 
   LOG_INFO("AES decrypting in ECB mode using key: " << hmr::hex::encode(key));
 
   AES_KEY aes_key;
   AES_set_decrypt_key(key_ptr, 128, &aes_key);
-  AES_decrypt(ciphertext_ptr, output.data(), &aes_key);
-  AES_decrypt(ciphertext_ptr+16, output.data()+16, &aes_key); // Need to loop through all the blocks
+
+  for (std::size_t offset = 0; offset < len; offset += 16)
+  {
+    AES_decrypt(ciphertext_ptr+offset, output.data()+offset, &aes_key);
+  }
 
   LOG_INFO(output.size());
 
@@ -76,9 +80,7 @@ void run()
 
   if (!decrypted.empty())
   {
-    LOG_INFO("Something happened!");
     auto result = std::string{};
-
     std::copy(std::begin(decrypted), std::end(decrypted), std::back_inserter(result));
 
     LOG_INFO(result);
